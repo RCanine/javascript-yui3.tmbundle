15f294fcfa243b3818d2d3157ac2e01d3824550d
Ryan Cannon <ryan@ryancannon.com>
2012-03-26 16:21:40 -0700

    adds YUI Doc, Base and Add snippets

--- Templates/YUI 3 JavaScript Module.tmTemplate/module.js
+++ Templates/YUI 3 JavaScript Module.tmTemplate/module.js
@@ -1,44 +1,13 @@
 /**
- *  ${TM_NEW_FILE_BASENAME}
- *
- *  Created by ${TM_FULLNAME} on ${TM_DATE}.
- *  Copyright (c) ${TM_YEAR} ${TM_ORGANIZATION_NAME}. All rights reserved.
+ * This is a description of my module
+ * 
+ * @module ${TM_NEW_FILE_BASENAME}
+ * @requires base-build, widget-base
  */
 YUI.add("${TM_NEW_FILE_BASENAME}", function (Y) {
-	// declare variables
-	var Superclass, mixins, instanceProperties, classProperties, MyModule;
-	
-	// The class your class inherits from
-	Superclass = Y.Base;
-	
-	// mixins to add
-	mixins = [];
-	
-	// instance methods and properties
-	instanceProperties = {
-		initializer: function (config) {
-			
-		},
-		destructor: function () {
-			
-		}
-	};
-	
-	// static properties
-	classProperties = {
-		ATTRS: {
-			
-		}
-	};
+	"use strict";
 
-	// generate the class
-	MyModule = Y.Base.create("video-base", Superclass, mixins, instanceProperties, classProperties);
-	
-	// expose the module publicly
-	Y.MyModule = MyModule;
-	
-}, "YUI_VERSION", {
-	requires: ["base-build", "base-base"],
-	skinnable: false,
-	group: "YOUR_GROUP"
+
+}, "${TM_YUI_VERSION}", {
+	requires: ["base-build", "widget-base"]
 });
